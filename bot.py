46f75a2814d4a3ed063070b66c9dc1b303825bf2
alistair-lilley <jklilley12@gmail.com>
2023-05-20 12:53:54 -0700

    Removed pycache, implemented killswitch, customized server messages, and restrcited dms to me only

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 import json
 import re
+import sys
+import os
 
 from dotenv import dotenv_values
 
@@ -9,47 +11,146 @@ from pinggroup import PingGroup
 config = dotenv_values(".env")
 
 TOKEN = config["TOKEN"]
+ALI = config["ALI"]
 
 intents = discord.Intents.all()
 intents.message_content = True
 
 dialogs = json.load(open("dialogues.json"))
+if ("custom_dialogs.json" not in os.listdir() or
+            os.stat("custom_dialogs.json").st_size == 0):
+    json.dump({}, open("custom_dialogs.json", 'w'))
+guild_vals = json.load(open("custom_dialogs.json"))
 
 class Bot(discord.Client):
 
     async def on_ready(self):
         print(f"WELCOME TO PINGER BOT: {self.user}")
         self.ping_group = PingGroup()
+        self.commands = {
+            "!help" : self.help,
+            "!ping" : self.ping,
+            "!stop" : self.stop,
+            "!pingmsg" : self.change_ping_msg,
+            "!heremsg" : self.change_here_msg,
+            "!maxtime" : self.max_time
+        }
 
     async def on_message(self, message):
-        if message.author == self.user:
+
+        if (message.author == self.user or
+             (not message.guild and message.author.id != ALI)):
             return
 
-        elif message.channel.name != "ping":
-            await message.channel.send(dialogs["wrong_channel"])
+        elif message.content == "!shutdown" and str(message.author.id) == ALI:
+            await message.channel.send(dialogs["shutting_down"])
+            sys.exit()
 
-        elif message.content.startswith("!ping"):
-            content = message.content.split(' ')
-            if len(content) < 2:
-                await message.channel.send(dialogs["missing_username"])
-            elif len(content) > 2:
-                await message.channel.send(dialogs["too_many_words"])
-            else:
-                cmd = content[0]
-                target_user = content[1]
-                await message.channel.send(re.sub(dialogs["user"], target_user,
-                                                  dialogs["starting_ping"]))
-                await self.ping_group.add_pinger(message.guild, message.channel,
-                                                target_user)
-
-        elif message.content.startswith("!stop"):
-            content = message.content.split(' ', 2)
-            cmd = content[0]
-            target_user = content[1]
-            self.ping_group.force_kill(target_user)
+        elif message.content.startswith('!'):
+            await self.commands[message.content.split()[0]](message)
 
         elif "here" in message.content.lower():
-            self.ping_group.user_arrived(message.author)
+            await self.here(message)
+    
+    async def help(self, message):
+        if message.channel.name != "ping":
+            await message.channel.send(dialogs["wrong_channel"])
+            return
+        helpmsg = open("help.txt").read()
+        await message.channel.send(helpmsg)
+
+    async def ping(self, message):
+        if message.channel.name != "ping":
+            await message.channel.send(dialogs["wrong_channel"])
+            return
+        content = message.content.split(' ')
+        if len(content) < 2:
+            await message.channel.send(dialogs["missing_username"])
+        elif len(content) > 2:
+            await message.channel.send(dialogs["too_many_words"])
+        else:
+            cmd = content[0]
+            target_user = content[1]
+            await message.channel.send(re.sub(dialogs["user"], target_user,
+                                                dialogs["starting_ping"]))
+            await self.ping_group.add_pinger(message.guild, message.channel,
+                                            target_user)
+    
+    async def stop(self, message):
+        if message.channel.name != "ping":
+            return
+        content = message.content.split(' ', 2)
+        cmd = content[0]
+        target_user = content[1]
+        self.ping_group.force_kill(target_user)
+
+    async def change_ping_msg(self, message):
+        if message.channel.name != "ping":
+            await message.channel.send(dialogs["wrong_channel"])
+            return
+        content = message.content.split(' ', 1)
+        if len(content[1]) > 500:
+            await message.channel.send(dialogs["message_too_long"])
+        else:
+            if str(message.guild.id) not in guild_vals:
+                guild_vals[str(message.guild.id)] = {
+                    "pingmsg": content[1],
+                    "heremsg": dialogs["defaults"]["heremsg"],
+                    "timeout": dialogs["defaults"]["timeout"]
+                    }
+            else:
+                guild_vals[str(message.guild.id)]["pingmsg"] = content[1]
+            json.dump(guild_vals, open("custom_dialogs.json", 'w'))
+            await message.channel.send(dialogs["message_changed"])
+        
+    async def change_here_msg(self, message):
+        if message.channel.name != "ping":
+            await message.channel.send(dialogs["wrong_channel"])
+            return
+        content = message.content.split(' ', 1)
+        if len(content[1]) > 500:
+            await message.channel.send(dialogs["message_too_long"])
+        else:
+            if str(message.guild.id) not in guild_vals:
+                guild_vals[str(message.guild.id)] = {
+                    "pingmsg": dialogs["defaults"]["pingmsg"],
+                    "heremsg": content[1],
+                    "timeout": dialogs["defaults"]["timeout"]
+                    }
+            else:
+                guild_vals[str(message.guild.id)]["heremsg"] = content[1]
+            json.dump(guild_vals, open("custom_dialogs.json", 'w'))
+            await message.channel.send(dialogs["message_changed"])
+        
+    async def max_time(self, message):
+        if message.channel.name != "ping":
+            await message.channel.send(dialogs["wrong_channel"])
+            return
+        content = message.content.split()
+        if len(content) != 2:
+            await message.channel.send(dialogs["no_timeout_value"])
+        elif not content[1].isdigit():
+            await message.channel.send(dialogs["timeout_must_be_int"])
+        elif not (1 < int(content[1]) < 15):
+            await message.channel.send(dialogs["too_long_or_short"])
+        else:
+            if str(message.guild.id) not in guild_vals:
+                guild_vals[str(message.guild.id)] = {
+                    "pingmsg": dialogs["defaults"]["pingmsg"],
+                    "heremsg": dialogs["defaults"]["heremsg"],
+                    "timeout": int(content[1])
+                    }
+            else:
+                guild_vals[str(message.guild.id)]["timeout"] = int(content[1])
+            json.dump(guild_vals, open("custom_dialogs.json", 'w'))
+            await message.channel.send(dialogs["maxtime_changed"])
+
+    async def here(self, message):
+        if message.channel.name != "ping":
+            return
+        self.ping_group.user_arrived(message.author)       
+
+
 
 
 if __name__ == "__main__":
--- pinger.py
+++ pinger.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 import json
 import re
+import os
 
 dialogs = json.load(open("dialogues.json"))
 
@@ -9,11 +10,19 @@ MINUTE = 60
 
 class Pinger:
 
-    def __init__(self, guild, channel, username, timeout_minutes):
+    def __init__(self, guild, channel, username):
+        if ("custom_dialogs.json" not in os.listdir() or
+                    os.stat("custom_dialogs.json").st_size == 0):
+            json.dump({}, open("custom_dialogs.json", 'w'))
+        self.guild_vals = json.load(open("custom_dialogs.json"))
         self.target_user = username
         self.guild = guild
         self.channel = channel
-        self.timeout_max = timeout_minutes * MINUTE
+        if self.guild in self.guild_vals:
+            self.timeout_max = \
+                self.guild_vals[str(self.guild.id)]["timeout"] * MINUTE
+        else:
+            self.timeout_max = dialogs["defaults"]["timeout"] * MINUTE
         self.stop_ping = False
         self.end_message = dialogs["timed_out"]
 
@@ -32,13 +41,21 @@ class Pinger:
     
     async def _ping_repeating(self, end):
         while time.time() < end and not self.stop_ping:
-            await self.channel.send(re.sub(dialogs["user"], self.target_user,
-                                           dialogs["game_time"]))
+            if str(self.guild.id) in self.guild_vals:
+                msg = self.guild_vals[str(self.guild.id)]["pingmsg"]
+            else:
+                msg = dialogs["defaults"]["pingmsg"]
+            pingmsg = re.sub(dialogs["user"], self.target_user,
+                                            dialogs["ping_message"])
+            await self.channel.send(msg + pingmsg)
             time.sleep(1)
         await self.channel.send(self.end_message)
     
     def im_here(self):
-        self.end_message = dialogs["here"]
+        if str(self.guild.id) in self.guild_vals:
+            self.end_message = self.guild_vals[str(self.guild.id)]["heremsg"]
+        else:
+            self.end_message = dialogs["defaults"]["heremsg"]
         self.stop_ping = True
     
     def force_stop(self):
